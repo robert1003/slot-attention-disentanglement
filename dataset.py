e75bb7075d06271a1e64fe27e3c8784e95128d9b
Andrew Stange <andrew.r.stange@gmail.com>
2023-03-19 16:07:08 -0400

    bring over dataset from main (2)

--- dataset.py
+++ dataset.py
@@ -1,16 +1,36 @@
 import os
 import random
 import json
-import glob
 import numpy as np
 from PIL import Image
 import torch
-from torchvision import transforms, datasets
+from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.dataloader import default_collate
 
+class PARTNET(Dataset):
+    def __init__(self, split='train'):
+        super(PARTNET, self).__init__()
 
+        assert split in ['train', 'val', 'test']
+        self.split = split
+        self.root_dir = your_path     
+        self.files = os.listdir(self.root_dir)
+        self.img_transform = transforms.Compose([
+               transforms.ToTensor()])
 
+    def __getitem__(self, index):
+        path = self.files[index]
+        image = Image.open(os.path.join(self.root_dir, path, "0.png")).convert("RGB")
+        image = image.resize((128 , 128))
+        image = self.img_transform(image)
+        sample = {'image': image}
+
+        return sample
+            
+    
+    def __len__(self):
+        return len(self.files)
 
 class CLEVR(Dataset):
     def __init__(self, split='train'):
@@ -34,14 +54,8 @@ class CLEVR(Dataset):
 
     def __len__(self):
         return len(self.files)
-    
-
-
 
 class MultiDSprites(Dataset):
-    """
-    Source: https://github.com/applied-ai-lab/genesis/blob/master/datasets/multid_config.py
-    """
     def __init__(self, split='train', unique=True):
         super(MultiDSprites, self).__init__()
 
@@ -67,7 +81,3 @@ class MultiDSprites(Dataset):
 
     def __len__(self):
         return len(self.data)
-
-
-
-    
