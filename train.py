e7dfa56d266a306dd3d4ee3dd8a595735c010f3e
robert1003 <robert881003@gmail.com>
2023-03-19 13:42:38 -0400

    add tensorboard to training code

--- train.py
+++ train.py
@@ -7,13 +7,21 @@ import time
 import datetime
 import torch.optim as optim
 import torch
+import torchvision
+
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
+from PIL import Image as Image, ImageEnhance
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument
-parser.add_argument('--model_dir', default='./tmp/model10.ckpt', type=str, help='where to save models' )
+parser.add_argument('--name', required=True, type=str, help='name of the experiment' )
+parser.add_argument('--model_dir', default='./checkpoint', type=str, help='where to save models' )
+parser.add_argument('--log_dir', default='./logs', type=str, help='where to save logs' )
+parser.add_argument('--image_dir', default='./images', type=str, help='where to save images' )
 parser.add_argument('--seed', default=0, type=int, help='random seed')
 parser.add_argument('--batch_size', default=16, type=int)
 parser.add_argument('--num_slots', default=7, type=int, help='Number of slots in Slot Attention.')
@@ -27,6 +35,8 @@ parser.add_argument('--num_workers', default=4, type=int, help='number of worker
 parser.add_argument('--num_epochs', default=1000, type=int, help='number of workers for loading data')
 
 opt = parser.parse_args()
+
+writer = SummaryWriter(os.path.join(opt.log_dir, opt.name))
 resolution = (128, 128)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -35,6 +45,13 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 train_set = MultiDSprites('train')
 model = SlotAttentionAutoEncoder(resolution, opt.num_slots, opt.num_iterations, opt.hid_dim).to(device)
 # model.load_state_dict(torch.load('./tmp/model6.ckpt')['model_state_dict'])
+
+# dataset for visualization
+test_set = MultiDSprites('test')
+test_batch_size = 16
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=test_batch_size, shuffle=True, num_workers=0)
+test_images = next(iter(test_loader))['image']
+test_images = test_images.to(device)
 
 criterion = nn.MSELoss()
 
@@ -78,10 +95,39 @@ for epoch in range(opt.num_epochs):
 
     total_loss /= len(train_dataloader)
 
+    writer.add_scalar('Loss/train', total_loss, epoch)
+    writer.add_scalar('Learning Rate', learning_rate, epoch)
+
     print ("Epoch: {}, Loss: {}, Time: {}".format(epoch, total_loss,
         datetime.timedelta(seconds=time.time() - start)))
 
     if not epoch % 10:
         torch.save({
             'model_state_dict': model.state_dict(),
-            }, opt.model_dir)
+            }, os.path.join(opt.model_dir, opt.name, 'checkpoint-{}.ckpt'.format(epoch)))
+
+        # visualize the reconstructions
+        model.eval()
+        recon_combineds, recons, masks, slots = model(test_images)
+
+        images_to_show = []
+        for i, image in enumerate(test_images):
+            image = image.cpu()
+            recon_combined = recon_combineds[i].cpu().detach()
+            recon = recons[i].permute(0,3,1,2).cpu().detach()
+            mask = masks[i].permute(0,3,1,2).cpu().detach()
+
+            images_to_show.append(image)
+            images_to_show.append(recon_combined)
+            
+            for j in range(7):
+                picture = recon[j] * mask[j] + (1 - mask[j])
+                images_to_show.append(picture)
+
+        images_to_show = torchvision.utils.make_grid(images_to_show, nrow=opt.num_slots+2)
+        print(images_to_show.shape)
+
+        writer.add_image('Reconstructions', images_to_show, epoch)
+        plt.figure(figsize=(15,2*test_batch_size))
+        plt.imshow(images_to_show.permute(1,2,0))
+        plt.savefig(os.path.join(opt.name, opt.image_dir, 'reconstructions-{}.png'.format(epoch)), bbox_inches='tight')
