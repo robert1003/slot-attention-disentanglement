127a04ab358a6fb46157ec74fee0ab7e6a6d5b71
Andrew Stange <andrew.r.stange@gmail.com>
2023-04-01 21:20:23 -0400

    COCO datasets for generating/using ViT embeddings

--- pytorch/dataset.py
+++ pytorch/dataset.py
@@ -7,6 +7,7 @@ import torch
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.dataloader import default_collate
+from torchvision.datasets import VisionDataset
 
 class PARTNET(Dataset):
     def __init__(self, split='train'):
@@ -81,3 +82,77 @@ class MultiDSprites(Dataset):
 
     def __len__(self):
         return len(self.data)
+
+
+class COCO2017(VisionDataset):
+    """
+    A dataset for generating ViT embeddings from the COCO 2017 dataset. Roughly
+    based on torchvision.datasets.CocoDetection, with adaptations to give user
+    access to filenames and pre-processed target masks. 
+
+    Dataset source: 
+    `wget http://images.cocodataset.org/zips/val2017.zip`
+    `wget http://images.cocodataset.org/zips/train2017.zip`
+    `wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip`
+
+    Val size: 778 MB, 5k images
+    Train size: 18 GB, ~118k images
+    """
+    def __init__(self, path='./data/coco', split='val', resolution=(224,224)):
+        # Largely taken from torchvision.datasets.CocoDetection 
+        assert split in ['train', 'val']
+        root = os.path.join(path, f"{split}2017")
+        annFile = os.path.join(path, f"annotations/instances_{split}2017.json")
+
+        super().__init__(root, None, None, None)
+        from pycocotools.coco import COCO
+        self.coco = COCO(annFile)
+        self.ids = list(sorted(self.coco.imgs.keys()))
+        self.resolution = resolution
+        
+        # Resize to (224,224) ignoring aspect ratio (DINOSAUR pg35/36, COCO 2017 object segmentation)
+        self.img_transform = transforms.Compose([transforms.Resize(size=resolution), transforms.ToTensor()])
+
+    def __getitem__(self, index):
+        id = self.ids[index]
+        image, name = self._load_image(id)
+
+        image = self.img_transform(image)
+        sample = {'image': image, 'src' : name}
+        return sample
+
+    def _load_image(self, id):
+        path = self.coco.loadImgs(id)[0]["file_name"]
+        return Image.open(os.path.join(self.root, path)).convert("RGB"), path
+
+    def _load_target(self, id):
+        return self.coco.loadAnns(self.coco.getAnnIds(id))
+
+    def __len__(self):
+        return len(self.ids)
+
+
+class COCO2017Embeddings(COCO2017):
+    """
+    A dataset for handling pre-generated ViT embeddings of the COCO 2017 dataset.
+    """
+    def __init__(self, data_path='./data/coco', embed_path='./data/coco/embedding', split='val', resolution=(224, 224)):
+        super().__init__(data_path, split, resolution)
+        self.embed_path = embed_path
+
+    def __getitem__(self, index):
+        # TODO: convert target into per-pixel labels, and then resize target as well for evaluation
+        id = self.ids[index]
+        image, _ = self._load_image(id)
+        #target = self._load_target(id)
+        embed = self._load_embedding(id)
+
+        image = self.img_transform(image)
+        sample = {'image': image, 'embedding': embed} #, 'target' : target}
+        return sample
+
+    def _load_embedding(self, id):
+        path = self.coco.loadImgs(id)[0]["file_name"].split('.')[0] + '.npy'
+        return torch.tensor(np.load(os.path.join(self.embed_path, path)))
+    
+
---
+++ pytorch/experiments.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import argparse
+import os
+import numpy as np
+from tqdm import tqdm
+import timm
+from timm.models.vision_transformer import VisionTransformer, checkpoint_seq
+
+from dataset import COCO2017
+
+
+
+class FrozenViTExperiment(VisionTransformer):
+    def __init__(self):
+        super().__init__()
+        self.head = nn.Identity()       # make compatible with ViT state dict
+
+    def forward(self, x):
+        """Taken from timm/models/vision_transformer.py VisionTransformer.forward_features
+        This experiment uses the final Transformer block output, without the final layer norm 
+        and with the CLS token removed."""
+        x = self.patch_embed(x)
+        x = self._pos_embed(x)
+        x = self.norm_pre(x)
+        if self.grad_checkpointing and not torch.jit.is_scripting():
+            x = checkpoint_seq(self.blocks, x)
+        else:
+            x = self.blocks(x)
+        # x = self.norm(x)
+
+        # CLS token is prepended, DINOSAUR experiment removes it
+        x = x[:, 1:, :]
+        # assert x.shape[1] == 196
+        return x
+
+
+def generate_coco_embeddings(args):
+    # Load pretrained weights (frozen ViT-B/16 encoder) and copy to a class with custom forward method
+    timm_model = timm.create_model('vit_base_patch16_224_dino', pretrained=True)
+    model = FrozenViTExperiment()
+    model.load_state_dict(timm_model.state_dict())
+    model.eval()
+
+    # Device management
+    device = torch.device("cpu")
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    elif torch.backends.mps.is_available():
+        # Mac M1 GPU support
+        device =  torch.device('mps')
+    model = model.to(device)
+
+    # Load COCO 2017 dataset
+    dataset = COCO2017(args.dataset_path, args.split)
+    loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True)
+
+    # Generate embeddings and store them to output destination
+    with torch.no_grad():
+        for batch in tqdm(loader):
+            out = model(batch['image'].to(device))
+
+            # Store embeddings for each image to individual files in output directory
+            # (Store embeddings separately so they can be matched with targets by filename)
+            for embed, name in zip(out, batch['src']):
+                out_name = os.path.join(args.output_dir, name.split('.')[0] + '.npy')
+                np.save(out_name, embed.cpu().numpy())
+                
+
+
+if __name__ == '__main__':
+    # Generate embeddings for COCO 2017 dataset from frozen ViT encoder for later use
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_dir', default='./tmp/model10.ckpt', type=str, help='where to save embeddings' )
+    parser.add_argument('--batch_size', default=16, type=int)
+    parser.add_argument('--num_workers', default=4, type=int, help='number of workers for loading data')
+    parser.add_argument('--dataset_path', default="./data/CLEVR_v1.0/images", type=str, help='path to COCO dataset')
+    parser.add_argument('--split', default='train', type=str)
+
+    generate_coco_embeddings(parser.parse_args())
+
+
